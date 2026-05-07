e9ad78f0982d0ef7066a67fd0187ad186e3b3218
ThaiTran31 <93335727+ThaiTran31@users.noreply.github.com>
2026-05-07 21:26:36 +0700

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.go
@@ -0,0 +1,35 @@
+type Solution struct{}
+
+func (s *Solution) Encode(strs []string) string {
+	sizes := make([]string, 0)
+	for _, str := range strs {
+		sizes = append(sizes, fmt.Sprintf("%d", len(str)))
+	}
+	res := strings.Join(sizes, ",")
+	res += "#"
+	for _, str := range strs {
+		res += str
+	}
+	return res
+}
+
+func (s *Solution) Decode(encoded string) []string {
+	idx := strings.Index(encoded, "#")
+	if idx == 0 {
+		return []string{}
+	}
+	res := make([]string, 0)
+	sizesStr := encoded[:idx]
+	decodedStr := encoded[idx+1:]
+	sizes := strings.Split(sizesStr, ",")
+	fmt.Println(sizesStr == "", decodedStr == "", len(sizes))
+	accumSize := 0
+	for _, sizeStr := range sizes {
+		size, _ := strconv.Atoi(sizeStr)
+		fmt.Println(decodedStr[accumSize:accumSize+size], decodedStr[accumSize:accumSize+size] == "")
+		res = append(res, decodedStr[accumSize:accumSize+size])
+		accumSize += size
+	}
+	fmt.Println(len(res))
+	return res
+}

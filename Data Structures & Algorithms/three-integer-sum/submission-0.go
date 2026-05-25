21fb7903d946bb66914a04c29374bf1936e07c47
ThaiTran31 <93335727+ThaiTran31@users.noreply.github.com>
2026-05-25 23:44:57 +0700

    Add: three-integer-sum - submission-0

---
+++ Data Structures & Algorithms/three-integer-sum/submission-0.go
@@ -0,0 +1,18 @@
+func threeSum(nums []int) [][]int {
+    res := map[[3]int]struct{}{}
+    sort.Ints(nums)
+    for i := 0; i < len(nums); i++ {
+        for j := i + 1; j < len(nums); j++ {
+            for k := j + 1; k < len(nums); k++ {
+                if nums[i]+nums[j]+nums[k] == 0 {
+                    res[[3]int{nums[i], nums[j], nums[k]}] = struct{}{}
+                }
+            }
+        }
+    }
+    var result [][]int
+    for triplet := range res {
+        result = append(result, []int{triplet[0], triplet[1], triplet[2]})
+    }
+    return result
+}
